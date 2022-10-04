fd14809de377b9fc51c8b29539ff766781346ace
William Rogers <105190715+williamrogersdev@users.noreply.github.com>
2022-10-04 17:00:26 -0500

    Create setting-up.py

---
+++ setting-up.py
@@ -0,0 +1,12 @@
+source /Users/williamrogers/pyworkshop/env/bin/act%                                    
+williamrogers@Williams-MBP pyworkshop % source /Users/williamrogers/pyworkshop/env/bin/
+activate
+(env) williamrogers@Williams-MBP pyworkshop % /Users/williamrogers/pyworkshop/env/bin/p
+ython
+Python 3.10.7 (v3.10.7:6cc6b13308, Sep  5 2022, 14:02:52) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> 3/4
+0.75
+
+
+>>> 
